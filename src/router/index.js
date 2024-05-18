0e5af8ed43ad4d6eb8b5e56e52767b088170c4b1
vmwebio <vmwebio@gmail.com>
2024-05-18 15:46:42 +0300

    add router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: '/',
+        name: 'Index',
+        component: Index
+      },
+    ]
+})
+
+export default router;
