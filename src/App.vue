8b3c732da8dd972006488c28d756d5b69e01a99c
vmwebio <vmwebio@gmail.com>
2024-05-18 16:46:51 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <script setup>
-
+import LayoutHeader from "@/components/layout/Header.vue";
 </script>
 
 <template>
-  <router-view/>
+  <div class="container">
+    <layout-header />
+    <div class="content">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @import "./styles/global.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  max-width: 1400px;
+  margin-left: 250px;
+  padding: 30px;
+  transition: 0.2s;
+  &_full {
+    margin-left: 0;
+  }
+}
 
 </style>
